20bafb0990780bf201a7910d25d96494edf3a5da
Aaron <amramirez1222@gmail.com>
2021-09-13 19:30:21 -1000

    created playground, style dummybox, have data folder
    playground is used to be able to test what is needed to be done. Currently what is being tested is being able to show data from an array of objects instead of the static info we have.  style dummybox to give direction of how it could possibly look. Maybe look into shadows. have data folder for the data we have now. will probably be moved when you get the database

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import "./App.css";
 /* import MyButtons from "./components/myButtons"; */
 import DropDown from "./components/dropDown";
+import DummyBox from "./components/DummyBox";
 /* import Modal from "./components/Modal"; */
 
-
 function App() {
-
   return (
-    <div className='grid'>
+    <div className="grid">
       <br />
-      <h1 className='grid justify-center text-3xl'>Registration Site</h1>
-      <DropDown/>
-      {/* <MyButtons Clicked={onclick} /> */}
-      <br />
-      {/*showModal && <Modal />*/}
-      {/*       <Modal trigger={true}></Modal> */}
+      <h1 className="grid justify-center text-3xl">Registration Site</h1>
+      <DropDown />
+      <DummyBox />
     </div>
   );
 }
